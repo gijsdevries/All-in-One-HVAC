658f4dc26b82aa489da35c91bbdccbdfa14eb97f
gijs <gijsdvries21@gmail.com>
2026-03-04 16:26:04 +0100

    cleanup automatic script

--- pythonFiles/serial_com.py
+++ pythonFiles/serial_com.py
@@ -6,19 +6,27 @@ try:
 
         while 1:
             ser.write(b"M0 oda_fan D0\r")
-            print("send command: M0 oda_fan D0")
+            print("--- send command: M0 oda_fan D0 ---")
+            line = ser.readline()
+            print(line.decode().strip() + "\n")
             time.sleep(5)
 
             ser.write(b"M0 oda_fan D50\r")
-            print("send command: M0 oda_fan D50")
+            print("--- send command: M50 oda_fan D50 ---")
+            line = ser.readline()
+            print(line.decode().strip() + "\n")
             time.sleep(5)
 
             ser.write(b"M0 oda_fan D100\r")
-            print("send command: M0 oda_fan D100")
+            print("--- send command: M100 oda_fan D100 ---")
+            line = ser.readline()
+            print(line.decode().strip() + "\n")
             time.sleep(5)
 
             ser.write(b"M0 oda_fan D50\r")
-            print("send command: M0 oda_fan D50")
+            print("--- send command: M50 oda_fan D50 ---")
+            line = ser.readline()
+            print(line.decode().strip() + "\n")
             time.sleep(5)
 
 
