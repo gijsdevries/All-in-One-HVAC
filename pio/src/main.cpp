d2375714a000f31b785fd8d4aaa448ef9f56bde5
Xander <xanderperry2003@gmail.com>
2026-04-07 14:56:00 +0200

    Implement first version of TH832X reading

--- pio/src/main.cpp
+++ pio/src/main.cpp
@@ -5,6 +5,15 @@
 #include "sdkconfig.h"
 #include <Arduino.h>
 
+#include <Wire.h>
+
+struct CTH832X_data {
+    float celcius;
+    float relative_humidity;
+};
+
+#define SENSOR_ADDR 0x44  
+
 #define BLINK_GPIO (gpio_num_t)CONFIG_BLINK_GPIO
 
 void blink_task(void *pvParameter)
@@ -23,13 +32,67 @@ void blink_task(void *pvParameter)
     }
 }
 
+CTH832X_data get_temp(void)
+{
+    Wire.beginTransmission(SENSOR_ADDR);
+    Wire.write(0x24);       
+    Wire.write(0x00);       
+    int error = Wire.endTransmission();
+    
+    if (error != 0) {
+        Serial.println("Command sending failed！");
+        CTH832X_data temp_hum;
+        temp_hum.celcius = 0;
+        temp_hum.relative_humidity = 0;
+        return temp_hum;
+    }
+
+    delay(60);              
+
+    
+    Wire.requestFrom(SENSOR_ADDR, 6);
+    if (Wire.available() == 6) {
+        // Read temperature data
+        uint16_t temp_raw = (Wire.read() << 8) | Wire.read();
+        Wire.read();         
+        
+        // Read humidity data
+        uint16_t humi_raw = (Wire.read() << 8) | Wire.read();
+        Wire.read();          
+
+        CTH832X_data temp_hum;
+        temp_hum.celcius = -45.0 + 175.0 * (temp_raw / 65535.0);
+        temp_hum.relative_humidity = 100.0 * (humi_raw / 65535.0);
+
+
+        Serial.print("temperature: ");
+        Serial.print(temp_hum.celcius, 2);
+        Serial.print("°C \thumidity: ");
+        Serial.print(temp_hum.relative_humidity, 2);
+        Serial.println(" %RH");
+
+        return temp_hum;
+    } 
+    else {
+        Serial.println("Data read failed！");
+        CTH832X_data temp_hum;
+        temp_hum.celcius = 0;
+        temp_hum.relative_humidity = 0;
+        return temp_hum;
+    }
+}
+
 void setup() {
     Serial.begin(115200);
+
+    Wire.begin();
+
     xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
     pinMode(BLINK_GPIO, OUTPUT);
 }
 
 void loop() {
-    Serial.println("Hello!");
-    delay(1000);
+    CTH832X_data test = get_temp();    
+
+    delay(500);   
 }
