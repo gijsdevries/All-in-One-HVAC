42096df668dea67603851b6bc868c083344ed578
gijs <gijsdvries21@gmail.com>
2026-04-13 13:06:47 +0200

    recieving temp data in home assistent

--- pio/src/main.cpp
+++ pio/src/main.cpp
@@ -16,121 +16,119 @@ const char topic[] = "/hello";
 
 QueueHandle_t dataQueue = NULL; //  Initialize dataQueue used for passing measurements to data uploader
 
-struct data_struct {    //  Struct for measurements
-    float temp;
-    float hum;
+struct data_struct //  Struct for measurements
+{
+  float temp;
+  float hum;
 };
 
 /*
-    SHT40Task
-    This function is used as a freeRTOS task, it measures temperature and relative 
-    humidity and adds these to the dataQueue
-*/
+   SHT40Task
+   This function is used as a freeRTOS task, it measures temperature and relative 
+   humidity and adds these to the dataQueue
+   */
 
 void SHT40Task(void *parameter)
 {
-    while(true)
+  while(true)
+  {
+    Wire.beginTransmission(SENSOR_ADDR);    //  Initializes I2C communication
+    Wire.write(0xFD);   //  Sends command for single high precision T & RH measurement
+
+    int error = Wire.endTransmission();
+    if (error != 0)
     {
-        Wire.beginTransmission(SENSOR_ADDR);    //  Initializes I2C communication
-        Wire.write(0xFD);   //  Sends command for single high precision T & RH measurement
-
-        int error = Wire.endTransmission();
-        if (error != 0) {
-            Serial.println("Command sending failed！");
-            continue;
-        }
-
-        vTaskDelay(60 / portTICK_PERIOD_MS);             
-
-        
-        Wire.requestFrom(SENSOR_ADDR, 6);
-        if (Wire.available() == 6) {
-            // Read temperature data
-            uint16_t temp_raw = (Wire.read() << 8) | Wire.read();
-            Wire.read();         
-            
-            // Read humidity data
-            uint16_t hum_raw = (Wire.read() << 8) | Wire.read();
-            Wire.read();          
-            
-            data_struct data;
-
-            data.temp = -45.0 + 175.0 * (temp_raw / 65535.0);
-            data.hum = -6 + 125 * (hum_raw / 65535.0);
-            if (data.hum > 100.0) data.hum = 100.0;
-            else if (data.hum < 0.0) data.hum = 0.0;
-
-            xQueueSend(dataQueue, &data, portMAX_DELAY);
-            
-            vTaskDelay(MEASUREMENT_INTERVAL / portTICK_PERIOD_MS);
-        } 
-        else {
-            Serial.println("Data read failed！");
-            continue;
-        }
+      Serial.println("Command sending failed！");
+      continue;
     }
+
+    vTaskDelay(60 / portTICK_PERIOD_MS);             
+
+
+    Wire.requestFrom(SENSOR_ADDR, 6);
+    if (Wire.available() == 6)
+    {
+      // Read temperature data
+      uint16_t temp_raw = (Wire.read() << 8) | Wire.read();
+      Wire.read();         
+
+      // Read humidity data
+      uint16_t hum_raw = (Wire.read() << 8) | Wire.read();
+      Wire.read();          
+
+      data_struct data;
+
+      data.temp = -45.0 + 175.0 * (temp_raw / 65535.0);
+      data.hum = -6 + 125 * (hum_raw / 65535.0);
+      if (data.hum > 100.0) data.hum = 100.0;
+      else if (data.hum < 0.0) data.hum = 0.0;
+
+      xQueueSend(dataQueue, &data, portMAX_DELAY);
+
+      vTaskDelay(MEASUREMENT_INTERVAL / portTICK_PERIOD_MS);
+    } 
+    else
+    {
+      Serial.println("Data read failed！");
+      continue;
+    }
+  }
 }
 
-//post data to certain topic
-void mqtt_post(void *pvParameter)
+void mqtt_post(void *parameter)
 {
-    while(1)
+  data_struct data;
+
+  while(true)
+  {
+    if (xQueueReceive(dataQueue, &data, portMAX_DELAY))
     {
-      client.publish(topic, "hi");
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      char buffer[50];
+      snprintf(buffer, sizeof(buffer), "%.2f", data.temp);
+
+      client.publish(topic, buffer);
+      printf("From queue: Temp: %.2f | Hum: %.2f\n", data.temp, data.hum);
+      vTaskDelay(pdMS_TO_TICKS(1000));
     }
+  }
 }
 
 //start wifi and mqtt connection and keep it alive
 void mqtt_connection(void *pvParameter)
 {
-    const char ssid[] = "Energielab";
-    const char pass[] = "Energie0238";
+  const char ssid[] = "Energielab";
+  const char pass[] = "Energie0238";
 
-    connect_wifi(ssid, pass);
-    connect_mqtt();
-    client.subscribe(topic);
+  connect_wifi(ssid, pass);
+  connect_mqtt();
+  client.subscribe(topic);
 
-    xTaskCreate(&mqtt_post, "mqtt_post", 8192, NULL, 5, NULL);
-    
-    while (1)
-    {
-      client.loop(); //this function should be called frequently to keep connection with broker alive
-      vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-}
+  xTaskCreate(mqtt_post, "mqtt_post", 8192, NULL, 5, NULL);
 
-void PrintTask(void *parameter)
-{
-    while(true)
-    {
-	data_struct data;
-        if (xQueueReceive(dataQueue, &data, portMAX_DELAY))
-        {
-            Serial.printf("From queue: Temp: %.2f | Hum: %.2f\n", data.temp, data.hum);
-        }
-    }
+  while (1)
+  {
+    client.loop(); //this function should be called frequently to keep connection with broker alive
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
 }
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    Wire.begin();
+  Wire.begin();
 
-    dataQueue = xQueueCreate(QUEUE_SIZE, sizeof(data_struct));
-    if (dataQueue == NULL)
-    {
-        Serial.println("Failed to create dataQueue.");
-        while(true);
-    }    
+  dataQueue = xQueueCreate(QUEUE_SIZE, sizeof(data_struct));
+  if (dataQueue == NULL)
+  {
+    Serial.println("Failed to create dataQueue.");
+    while(true);
+  }    
 
-    xTaskCreatePinnedToCore(SHT40Task, "SHT40Task", 3000, NULL, 1, NULL, 1);
-    xTaskCreatePinnedToCore(PrintTask, "PrintTask", 3000, NULL, 1, NULL, 1);
-
-    xTaskCreate(&mqtt_connection, "mqtt_connection", 8192, NULL, 5, NULL);
+  xTaskCreate(SHT40Task, "SHT40Task", 8192, NULL, 5, NULL);
+  xTaskCreate(&mqtt_connection, "mqtt_connection", 8192, NULL, 5, NULL);
 }
 
 void loop() 
 {
-    vTaskDelay(pdMS_TO_TICKS(1000));
+  vTaskDelay(pdMS_TO_TICKS(1000));
 }
