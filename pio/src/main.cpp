15c64be70d7f682b3e00950e10b928d6e591918a
gijs <gijsdvries21@gmail.com>
2026-04-13 16:17:27 +0200

    cleanupped

--- pio/src/main.cpp
+++ pio/src/main.cpp
@@ -10,76 +10,7 @@
 #include <mqtt.h>
 #include <sensor.h>
 
-#define SENSOR_ADDR 0x44    // SHT40 I2C address
-#define QUEUE_SIZE 2    // Size of data queue
-#define MEASUREMENT_INTERVAL 1000
-
-
 #define BLINK_GPIO (gpio_num_t)2
-
-QueueHandle_t dataQueue = NULL; //  Initialize dataQueue used for passing measurements to data uploader
-
-/*
-   SHT40Task
-   This function is used as a freeRTOS task, it measures temperature and relative 
-   humidity and adds these to the dataQueue
-   */
-
-void SHT40Task(void *parameter)
-{
-    Wire.begin();
-
-    dataQueue = xQueueCreate(QUEUE_SIZE, sizeof(data_struct));
-    if (dataQueue == NULL)
-    {
-	Serial.println("Failed to create dataQueue.");
-	vTaskDelete(NULL);
-    }    
-
-    data_struct data;
-
-    while(true)
-    {
-	Wire.beginTransmission(SENSOR_ADDR);    //  Initializes I2C communication
-	Wire.write(0xFD);   //  Sends command for single high precision T & RH measurement
-
-	int error = Wire.endTransmission();
-	if (error != 0)
-	{
-	    Serial.println("Command sending failed！");
-	    vTaskDelay(100 / portTICK_PERIOD_MS);             
-	    continue;
-	}
-
-	vTaskDelay(60 / portTICK_PERIOD_MS);             
-
-	Wire.requestFrom(SENSOR_ADDR, 6);
-	if (Wire.available() == 6)
-	{
-	    // Read temperature data
-	    uint16_t temp_raw = (Wire.read() << 8) | Wire.read();
-	    Wire.read();         
-
-	    // Read humidity data
-	    uint16_t hum_raw = (Wire.read() << 8) | Wire.read();
-	    Wire.read();          
-
-	    data.temp = -45.0 + 175.0 * (temp_raw / 65535.0);
-	    data.hum = -6 + 125 * (hum_raw / 65535.0);
-	    if (data.hum > 100.0) data.hum = 100.0;
-	    else if (data.hum < 0.0) data.hum = 0.0;
-
-	    xQueueSend(dataQueue, &data, portMAX_DELAY);
-
-	    vTaskDelay(MEASUREMENT_INTERVAL / portTICK_PERIOD_MS);
-	} 
-	else
-	{
-	    Serial.println("Data read failed！");
-	    continue;
-	}
-    }
-}
 
 // task that sends mqtt data to home assistent
 void mqtt_post(void *parameter)
